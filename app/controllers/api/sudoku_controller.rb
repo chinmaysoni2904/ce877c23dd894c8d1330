57cf348411c4a549c4908c85ae0b01b7b3f8bef8
Chinmay <cpct000@gmail.com>
2021-03-09 17:09:03 +0530

    parse json

--- app/controllers/api/sudoku_controller.rb
+++ app/controllers/api/sudoku_controller.rb
@@ -1,6 +1,7 @@
 class Api::SudokuController < ApplicationController
   def sudoku
     input = params[:data]
+    input = JSON.parse(params[:data]) if params[:data].class == String
     return render :json => {status: 500, message: "Input is not given"} if input.blank?
     response = Sudoku.new.solve(input)
     render :json => {solution: response}
